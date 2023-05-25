b0af7b8e90103cbb24d0d9c84a18d2e8cd3cf97e
gabrielmf07 <plaeis260@gmail.com>
2023-05-25 04:36:48 -0500

    lab10

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from tienda.models import Categoria,Producto
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+        
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ['id','nombre','precio']
+        
+        
